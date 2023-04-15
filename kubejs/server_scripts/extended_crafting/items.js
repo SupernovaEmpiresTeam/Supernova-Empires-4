2e2663755a2fe39ad7cc2903b7f70aaf9a939207
Filma Bem <filmab.emtv2@gmail.com>
2023-04-15 21:01:52 +0100

    Updates configs to make sure everything works as it should

---
+++ kubejs/server_scripts/extended_crafting/items.js
@@ -0,0 +1,8 @@
+
+
+onEvent('recipes', event => {
+    
+    event.shapeless('extendedcrafting:luminessence', ['minecraft:redstone', 'kubejs:bluestone', '2x minecraft:glowstone', 'minecraft:gunpowder', 'kubejs:light_bluestone', 'kubejs:greenstone'])
+    
+
+  })
--- kubejs/startup_scripts/items/ingot_variations.js
+++ kubejs/startup_scripts/items/ingot_variations.js
@@ -4,7 +4,6 @@ onEvent('item.registry', event => {
     // Bluestone
 
     event.create('bluestone').displayName('Bluestone')
-    event.create('bluestone_seed').displayName('Bluestone Seed')
     event.create('bluestone_gear').displayName('Bluestone Gear')
     event.create('bluestone_plate').displayName('Bluestone Plate')
     event.create('bluestone_rod').displayName('Bluestone Rod')
@@ -12,7 +11,6 @@ onEvent('item.registry', event => {
     // Light Bluestone
 
     event.create('light_bluestone').displayName('Light Bluestone')
-    event.create('light_bluestone_seed').displayName('Light Bluestone Seed')
     event.create('light_bluestone_gear').displayName('Light Bluestone Gear')
     event.create('light_bluestone_plate').displayName('Light Bluestone Plate')
     event.create('light_bluestone_rod').displayName('Light Bluestone Rod')
@@ -20,148 +18,128 @@ onEvent('item.registry', event => {
     // Greenstone
 
     event.create('greenstone').displayName('Greenstone')
-    event.create('greenstone_seed').displayName('Greenstone Seed')
     event.create('greenstone_gear').displayName('Greenstone Gear')
     event.create('greenstone_plate').displayName('Greenstone Plate')
     event.create('greenstone_rod').displayName('Greenstone Rod')
 
     // Steel
 
-    event.create('steel_seed').displayName('Steel Seed')
     event.create('steel_gear').displayName('Steel Gear')
     event.create('steel_plate').displayName('Steel Plate')
     event.create('steel_rod').displayName('Steel Rod')
 
     //  Dark Steel
 
-    event.create('dark_steel_seed').displayName('Steel Seed')
-    event.create('dark_steel_gear').displayName('Steel Gear')
-    event.create('dark_steel_plate').displayName('Steel Plate')
-    event.create('dark_steel_rod').displayName('Steel Rod')
+    event.create('dark_steel_seed').displayName('Dark Steel Seed')
+    event.create('dark_steel_gear').displayName('Dark Steel Gear')
+    event.create('dark_steel_plate').displayName('Dark Steel Plate')
+    event.create('dark_steel_rod').displayName('Dark Steel Rod')
 
     // Draconium
 
-    event.create('draconium_seed').displayName('Draconium Seed')
     event.create('draconium_gear').displayName('Draconium Gear')
     event.create('draconium_plate').displayName('Draconium Plate')
     event.create('draconium_rod').displayName('Draconium Rod')
 
     // Copper
 
-    event.create('copper_seed').displayName('Copper Seed')
     event.create('copper_gear').displayName('Copper Gear')
     event.create('copper_plate').displayName('Copper Plate')
     event.create('copper_rod').displayName('Copper Rod')
 
     // Bronze
-
-    event.create('bronze_seed').displayName('Bronze Seed')
     event.create('bronze_gear').displayName('Bronze Gear')
     event.create('bronze_plate').displayName('Bronze Plate')
     event.create('bronze_rod').displayName('Bronze Rod')
 
     // Tin
 
-    event.create('tin_seed').displayName('Tin Seed')
     event.create('tin_gear').displayName('Tin Gear')
     event.create('tin_plate').displayName('Tin Plate')
     event.create('tin_rod').displayName('Tin Rod')
 
     // Nickel
 
-    event.create('nickel_seed').displayName('Nickel Seed')
     event.create('nickel_gear').displayName('Nickel Gear')
     event.create('nickel_plate').displayName('Nickel Plate')
     event.create('nickel_rod').displayName('Nickel Rod')
 
     // Constantan
 
-    event.create('constantan_seed').displayName('Constantan Seed')
     event.create('constantan_gear').displayName('Constantan Gear')
     event.create('constantan_plate').displayName('Constantan Plate')
     event.create('constantan_rod').displayName('Constantan Rod')
 
     // Lead
 
-    event.create('lead_seed').displayName('Lead Seed')
     event.create('lead_gear').displayName('Lead Gear')
     event.create('lead_plate').displayName('Lead Plate')
     event.create('lead_rod').displayName('Lead Rod')
 
     // Silver
 
-    event.create('silver_seed').displayName('Silver Seed')
     event.create('silver_gear').displayName('Silver Gear')
     event.create('silver_plate').displayName('Silver Plate')
     event.create('silver_rod').displayName('Silver Rod')
 
     // Electrum
 
-    event.create('electrum_seed').displayName('Electrum Seed')
     event.create('electrum_gear').displayName('Electrum Gear')
     event.create('electrum_plate').displayName('Electrum Plate')
     event.create('electrum_rod').displayName('Electrum Rod')
 
     // Uranium
 
-    event.create('uranium_seed').displayName('Uranium Seed')
     event.create('uranium_gear').displayName('Uranium Gear')
     event.create('uranium_plate').displayName('Uranium Plate')
     event.create('uranium_rod').displayName('Uranium Rod')
 
     // Osmium
 
-    event.create('osmium_seed').displayName('Osmium Seed')
     event.create('osmium_gear').displayName('Osmium Gear')
     event.create('osmium_plate').displayName('Osmium Plate')
     event.create('osmium_rod').displayName('Osmium Rod')
 
     // Fbmodium
 
-    event.create('fbmodium_seed').displayName('Fbmodium Seed')
+
     event.create('fbmodium_gear').displayName('Fbmodium Gear')
     event.create('fbmodium_plate').displayName('Fbmodium Plate')
     event.create('fbmodium_rod').displayName('Fbmodium Rod')
 
-    event.create('nether_infused_fbmodium_seed').displayName('Nether Infused Fbmodium Seed')
+
     event.create('nether_infused_fbmodiumgear').displayName('Nether Infused Fbmodium Gear')
     event.create('nether_infused_fbmodium_plate').displayName('Nether Infused Fbmodium Plate')
     event.create('nether_infused_fbmodium_rod').displayName('Nether Infused Fbmodium Rod')
 
-    event.create('end_infused_fbmodium_seed').displayName('End Infused Fbmodium Seed')
     event.create('end_infused_fbmodium_gear').displayName('End Infused Fbmodium Gear')
     event.create('end_infused_fbmodium_plate').displayName('End Infused Fbmodium Plate')
     event.create('end_infused_fbmodium_rod').displayName('End Infused Fbmodium Rod')
 
-    event.create('ultimate_infused_fbmodium_seed').displayName('Ultimate Infused Fbmodium Seed')
     event.create('ultimate_infused_fbmodium_gear').displayName('Ultimate Infused Fbmodium Gear')
     event.create('ultimate_infused_fbmodium_plate').displayName('Ultimate Infused FbmodiumPlate')
     event.create('ultimate_infused_fbmodium_rod').displayName('Ultimate Infused Fbmodium Rod')
 
     // Blue Iron
 
-    event.create('blue_iron_seed').displayName('Blue Iron Seed')
     event.create('blue_iron_gear').displayName('Blue Iron Gear')
     event.create('blue_iron_plate').displayName('Blue Iron Plate')
     event.create('blue_iron_rod').displayName('Blue Iron Rod')
 
     // Bluestone
 
-    event.create('bluestone_seed').displayName('Bluestone Seed')
     event.create('bluestone_gear').displayName('Bluestone Gear')
     event.create('bluestone_plate').displayName('Bluestone Plate')
     event.create('bluestone_rod').displayName('Bluestone Rod')
 
     // Light Bluestone
 
-    event.create('light_bluestone_seed').displayName('Light Bluestone Seed')
     event.create('light_bluestone_gear').displayName('Light Bluestone Gear')
     event.create('light_bluestone_plate').displayName('Light Bluestone Plate')
     event.create('light_bluestone_rod').displayName('Light Bluestone Rod')
 
     // Greenstone
 
-    event.create('greenstone_seed').displayName('Greenstone Seed')
     event.create('greenstone_gear').displayName('Greenstone Gear')
     event.create('greenstone_plate').displayName('Greenstone Plate')
     event.create('greenstone_rod').displayName('Greenstone Rod')
