8de681b8450d8c8c1a84199fca084c741ad37494
Filma Bem <filmab.emtv2@gmail.com>
2023-04-15 16:04:59 +0100

    Ported universal upgrade

--- kubejs/startup_scripts/items/crafting_items.js
+++ kubejs/startup_scripts/items/crafting_items.js
@@ -9,4 +9,7 @@ onEvent('item.registry', event => {
     event.create('module_base').displayName('Module Base')
     event.create('machine_casing').displayName('Machine Casing')
     event.create('energy_module').displayName('Energy Module')
+    event.create('universal_upgrade_core').displayName('Universal Upgrade Core')
+    event.create('universal_upgrade_cover').displayName('Universal Upgrade Cover')
+    event.create('universal_upgrade').displayName('Universal Upgrade')
 })
