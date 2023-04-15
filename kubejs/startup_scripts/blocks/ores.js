d5a04fe58e7deb7b032e6cc6da6f746f43a28215
Filma Bem <filmab.emtv2@gmail.com>
2023-04-15 17:04:29 +0100

    Ported Ores from contenttweaker to kubejs

--- kubejs/startup_scripts/blocks/ores.js
+++ kubejs/startup_scripts/blocks/ores.js
@@ -2,5 +2,36 @@
 
 onEvent('block.registry', event => {
 
+    // Overworld
+
+    event.create('bluestone_ore').material('metal').hardness(5.3).displayName('Bluestone Ore')
+    event.create('light_bluestone_ore').material('metal').hardness(5.3).displayName('Light Bluestone Ore')
+    event.create('greenstone_ore').material('metal').hardness(5.3).displayName('Greenstone Ore')
+
+    // Nether
+
+    event.create('nether_coal_ore').material('metal').hardness(5.3).displayName('Nether Coal Ore')
+    event.create('nether_copper_ore').material('metal').hardness(5.3).displayName('Nether Copper Ore')
+    event.create('nether_diamond_ore').material('metal').hardness(5.3).displayName('Nether Diamond Ore')
+    event.create('nether_emerald_ore').material('metal').hardness(5.3).displayName('Nether Emerald Ore')
+    event.create('nether_iron_ore').material('metal').hardness(5.3).displayName('Nether Iron Ore')
+    event.create('nether_lapis_ore').material('metal').hardness(5.3).displayName('Nether Lapis Ore')
+    event.create('nether_lead_ore').material('metal').hardness(5.3).displayName('Nether Lead Ore')
+    event.create('nether_osmium_ore').material('metal').hardness(5.3).displayName('Nether Osmium Ore')
+    event.create('nether_redstone_ore').material('metal').hardness(5.3).displayName('Nether Redstone Ore')
+    event.create('nether_tin_ore').material('metal').hardness(5.3).displayName('Nether Tin Ore')
+
+    // End
+
+    event.create('end_coal_ore').material('metal').hardness(5.3).displayName('End Coal Ore')
+    event.create('end_copper_ore').material('metal').hardness(5.3).displayName('End Copper Ore')
+    event.create('end_diamond_ore').material('metal').hardness(5.3).displayName('End Diamond Ore')
+    event.create('end_emerald_ore').material('metal').hardness(5.3).displayName('End Emerald Ore')
+    event.create('end_iron_ore').material('metal').hardness(5.3).displayName('End Iron Ore')
+    event.create('end_lapis_ore').material('metal').hardness(5.3).displayName('End Lapis Ore')
+    event.create('end_lead_ore').material('metal').hardness(5.3).displayName('End Lead Ore')
+    event.create('end_osmium_ore').material('metal').hardness(5.3).displayName('End Osmium Ore')
+    event.create('end_redstone_ore').material('metal').hardness(5.3).displayName('End Redstone Ore')
+    event.create('end_tin_ore').material('metal').hardness(5.3).displayName('End Tin Ore')
     
-  })
+})
