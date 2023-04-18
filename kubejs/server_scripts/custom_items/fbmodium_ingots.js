9598861153d77612eb14bc9731202e2d57d8c0b1
Filma Bem <filmab.emtv2@gmail.com>
2023-04-18 16:25:48 +0100

    Ported Fbmodium Ingots' recipes to kubejs

---
+++ kubejs/server_scripts/custom_items/fbmodium_ingots.js
@@ -0,0 +1,281 @@
+onEvent('recipes', event => {
+    
+    event.custom({
+        type: 'extendedcrafting:combination',
+        powerCost: 1000000,
+        input: {
+            item: 'projecte:klein_star_omega'
+        },
+        ingredients : [
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:gold\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:gold\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:gold\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:gold\"}"
+            },
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:bluestone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:bluestone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:bluestone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:bluestone\"}"
+            },
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:blue_iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:blue_iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:blue_iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:blue_iron\"}"
+            },
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:iron\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:iron\"}"
+            },
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:supremium\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:supremium\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:supremium\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:supremium\"}"
+            },
+
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:redstone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:redstone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:redstone\"}"
+            },
+            {
+                "type": "forge:nbt",
+                "item": "extendedcrafting:singularity",
+                "count": 1,
+                "nbt": "{Id:\"extendedcrafting:redstone\"}"
+            },
+        ],
+        result: {
+            item: "kubejs:fbmodium_ingot",
+            count: 4
+        }
+    })
+
+    event.custom({
+        type: 'extendedcrafting:combination',
+        powerCost: 2000000,
+        input: {
+            item: 'kubejs:fbmodium_ingot'
+        },
+        ingredients : [
+            {
+                "item": "minecraft:nether_star"
+              },
+              {
+                "item": "minecraft:nether_star"
+              },
+              {
+                "item": "minecraft:nether_star"
+              },
+              {
+                "item": "minecraft:nether_star"
+              },
+              {
+                "item": "minecraft:netherite_ingot"
+              },
+              {
+                "item": "minecraft:netherite_ingot"
+              },
+              {
+                "item": "minecraft:netherite_ingot"
+              },
+              {
+                "item": "minecraft:netherite_ingot"
+              }
+        ],
+        result: {
+            item: "kubejs:nether_infused_fbmodium_ingot",
+            count: 1
+        }
+    })
+
+    event.custom({
+        type: 'extendedcrafting:combination',
+        powerCost: 2000000,
+        input: {
+            item: 'kubejs:nether_infused_fbmodium_ingot'
+        },
+        ingredients : [
+            {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "tag": "forge:ores/draconium"
+              },
+              {
+                "tag": "forge:ores/draconium"
+              },
+              {
+                "tag": "forge:ores/draconium"
+              },
+              {
+                "tag": "forge:ores/draconium"
+              }
+        ],
+        result: {
+            item: "kubejs:end_infused_fbmodium_ingot",
+            count: 1
+        }
+    })
+
+    event.custom({
+        type: 'extendedcrafting:combination',
+        powerCost: 2500000,
+        input: {
+            item: 'kubejs:end_infused_fbmodium_ingot'
+        },
+        ingredients : [
+            {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "extendedcrafting:ender_star"
+              },
+              {
+                "item": "draconicevolution:dragon_heart"
+              },
+              {
+                "item": "draconicevolution:dragon_heart"
+              },
+              {
+                "item": "draconicevolution:dragon_heart"
+              },
+              {
+                "item": "draconicevolution:dragon_heart"
+              }
+        ],
+        result: {
+            item: "kubejs:ultimate_infused_fbmodium_ingot",
+            count: 1
+        }
+    })
+
+  })
