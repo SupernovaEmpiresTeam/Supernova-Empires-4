dc687e757dadf5d61c268558efd1a463917980bc
Filma Bem <filmab.emtv2@gmail.com>
2023-04-18 16:48:46 +0100

    Ported Digital Miner recipe to kubejs

---
+++ kubejs/server_scripts/mekanism/digital_miner.js
@@ -0,0 +1,46 @@
+onEvent('recipes', event => {
+    
+    // Helmet
+
+    event.custom({
+        type: 'extendedcrafting:shaped_table',
+        pattern: [
+            "AAAAAAAAA",
+            "BBBBBBBBB",
+            "AAAAAAAAA",
+            "DDDFFFDDD",
+            "EEEFGFEEE",
+            "DDDFFFDDD",
+            "BBBBBBBBB",
+            "AAAAAAAAA",
+            "BBBBBBBBB"
+          ],
+        key: {
+            "A": {
+                "item": "appliedenergistics2:fluix_crystal"
+            },
+            "B": {
+                "item": "cagedmobs:star_infused_netherite_ingot"
+            },
+            "C": {
+                "item": "extendedcrafting:enhanced_ender_ingot"
+            },
+            "D": {
+                "item": "extendedcrafting:ender_star"
+            },
+            "E": {
+                "item": "mekanism:logistical_sorter"
+            },
+            "F": {
+                "item": "mekanism:robit"
+            },
+            "G": {
+                "item": "rftoolsbuilder:builder"
+            }
+        },
+        result: {
+            item: 'mekanism:digital_miner'
+        }
+    })
+
+  })
