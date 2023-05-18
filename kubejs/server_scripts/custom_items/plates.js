623702476b5ef9664302adbbd2e5a0da8ea80e75
Filma Bem <filmab.emtv2@gmail.com>
2023-05-18 13:04:17 +0100

    Fixed plate recipes

--- kubejs/server_scripts/custom_items/plates.js
+++ kubejs/server_scripts/custom_items/plates.js
@@ -8,91 +8,91 @@ onEvent('recipes', event => {
 
     event.shapeless('kubejs:awakened_draconium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:awakened_draconium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:osmium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:osmium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:copper_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:copper_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:bronze_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:bronze_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:aluminium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:aluminium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:steel_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:steel_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:tin_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:tin_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:nickel_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:nickel_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:lead_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:lead_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:silver_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:silver_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:electrum_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:electrum_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:uranium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:uranium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:dark_steel_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:dark_steel_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:constantan_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:constantan_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:blue_iron_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:blue_iron_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:fbmodium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:fbmodium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:nether_infused_fbmodium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:nether_infused_fbmodium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:end_infused_fbmodium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:end_infused_fbmodium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:ultimate_infused_fbmodium_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:ultimate_infused_fbmodium_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:bluestone_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:bluestone_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:light_bluestone_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:light_bluestone_ingot'
-    ]),
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer'),
 
     event.shapeless('kubejs:greenstone_plate', [
         Item.of('kubejs:hammer').ignoreNBT(), 'kubejs:greenstone_ingot'
-    ])
+    ]).damageIngredient(0).keepIngredient('kubejs:hammer')
 
 })
 
--- kubejs/startup_scripts/items/crafting_items.js
+++ kubejs/startup_scripts/items/crafting_items.js
@@ -6,6 +6,7 @@ onEvent('item.registry', event => {
 
     event.create('disk_cable').displayName('Disk Cable')
     event.create('disk_casing').displayName('Disk Casing')
+    event.create('logic_board').displayName('Logic Board')
     event.create('module_base').displayName('Module Base')
     event.create('machine_casing').displayName('Machine Casing')
     event.create('energy_module').displayName('Energy Module')
@@ -13,6 +14,7 @@ onEvent('item.registry', event => {
     event.create('universal_upgrade_cover').displayName('Universal Upgrade Cover')
     event.create('universal_upgrade').displayName('Universal Upgrade')
     event.create('silicon').displayName('Silicon')
-    event.create('hammer').displayName('Hammer').maxDamage(2000)
+    event.create('hammer').type('shovel').tier('stone').displayName('Hammer').maxDamage(2000)
+    event.create('transmutation_stone').displayName('Transmutation Stone').maxDamage(5000)
 
 })
